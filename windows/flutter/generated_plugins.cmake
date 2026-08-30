74805214e45d7e21bae0a53a22d92bb280b7d49d
GamentaNP <242171145+GamentaNP@users.noreply.github.com>
2026-08-31 03:48:05 +0545

    chore: task plan ledger + windows plugin registrant sync

--- windows/flutter/generated_plugins.cmake
+++ windows/flutter/generated_plugins.cmake
@@ -6,7 +6,9 @@ list(APPEND FLUTTER_PLUGIN_LIST
   app_links
   connectivity_plus
   file_selector_windows
+  firebase_auth
   firebase_core
+  firebase_database
   firebase_remote_config
   flutter_secure_storage_windows
   local_auth_windows
